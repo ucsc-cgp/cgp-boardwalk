d36a8fb41d8214ca4c3d342851ec32999367f981
Charles Overbeck <coverbec@ucsc.edu>
2018-09-12 18:25:50 -0700

    Redirect to FireCloud instead of invoking API (fixes #11)
    Previously, we were calling FireCloud API to display our own UI
    to pick a FC workspace, and then POSTing the BDBag we were generating
    to FireCloud using the FC APIs.
    
    With this PR, we instead simply redirect to Saturn, the new FC API,
    passing it the URL of the BDBag as a query parameter.
    
    1. Got rid of the old code, which was not using ngrx
    2. Call the export endpoint with a new parameter value, `bdbag`,
    that returns an URL to the generated BDBag.
    3. When we get the BDBag url in response, open a new window
    in Saturn, passing the url as a query parameter. Actually we open
    the new window without a URL to begin with, and then set the url --
    this is because browsers won't let you open a new window in
    response to an Ajax response; so open it before the request, then
    change the href in the response. You do end up with a blank window
    briefly, but the call seems pretty quick so far.
    
    Todo: error handling.

---
+++ spa/src/app/files/_ngrx/file-export/file-export.actions.ts
@@ -0,0 +1,27 @@
+import { Action } from "@ngrx/store";
+import { WorkspaceDescriptor } from "./file-export.state";
+
+export class FileExportManifestRequestAction implements Action {
+    public static ACTION_TYPE = "FILE.MANIFEST_EXPORT_REQUEST";
+    public readonly type = FileExportManifestRequestAction.ACTION_TYPE;
+    constructor() {
+    }
+}
+
+export class FileExportManifestSuccessAction implements Action {
+    public static ACTION_TYPE = "FILE.MANIFEST_EXPORT_SUCCESS";
+    public readonly type = FileExportManifestSuccessAction.ACTION_TYPE;
+    constructor(public fireCloudUrl: string) {
+    }
+}
+
+export class FileExportManifestErrorAction implements Action {
+    public static ACTION_TYPE = "FILE.MANIFEST_EXPORT_ERROR";
+    public readonly type = FileExportManifestErrorAction.ACTION_TYPE;
+    constructor(public errorReason: string) {
+    }
+}
+
+export type All = FileExportManifestRequestAction
+    | FileExportManifestSuccessAction
+    | FileExportManifestErrorAction;

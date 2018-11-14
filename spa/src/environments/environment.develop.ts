68eb677036f79e956266a9277f482b9052a0170e
noopdog <dave@clevercanary.com>
2018-11-15 06:32:53 +1000

    set data and auth urls to charles to test

--- spa/src/environments/environment.develop.ts
+++ spa/src/environments/environment.develop.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    dataURL: "https://commons.ucsc-cgp-dev.org",
+    dataURL: "https://charles.ucsc-cgp-dev.org",
     portalURL: "https://dev.commons.ucsc-cgp-dev.org",
-    authURL: "https://commons.ucsc-cgp-dev.org"
+    authURL: "https://charles.ucsc-cgp-dev.org"
 };
